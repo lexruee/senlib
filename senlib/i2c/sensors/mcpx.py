08a38cf683a1f7abe625fc4142cf2fa2a3be73d2
Alexander Rüedlinger <a.rueedlinger@gmail.com>
2017-10-22 23:32:33 +0200

    add mcp9808 driver
    Signed-off-by: Alexander Rüedlinger <a.rueedlinger@gmail.com>

---
+++ senlib/i2c/sensors/mcpx.py
@@ -0,0 +1,63 @@
+# -*- coding: utf-8 -*-
+
+__author__ = 'Alexander Rüedlinger'
+__all__ = ('MCP9808')
+
+from senlib.core.i2c import Sensor as I2CSensor
+
+
+class MCP9808(I2CSensor):
+    """
+    This is a software driver implementation for the Microchip MCP9808 temperature sensor
+    for use with Raspberry Pi computers.
+    """
+
+    DRIVER_NAME = 'mcp9808'
+
+    ADDR = 0x18
+    DEFAULT_ADDR = ADDR
+
+    REG_CONFIG = 0x01
+
+    REG_ALERT = 0x04
+    REG_TMP = 0x05
+    REG_DEVICE_ID = 0x07
+    REG_RES = 0x08
+
+    def __init__(self, i2c_ctrl, addr=DEFAULT_ADDR):
+        super(MCP9808, self).__init__(i2c_ctrl, addr)
+        self.id = self._read_device_id()
+        self._temperature = 0.0
+
+    @classmethod
+    def driver_name(cls):
+        return cls.DRIVER_NAME
+
+    @classmethod
+    def default_addr(cls):
+        return cls.DEFAULT_ADDR
+
+    def _read_device_id(self):
+        return self._i2c_ctrl.read_byte_data(self.addr, self.REG_DEVICE_ID)
+
+    def read_temperature(self):
+        data = self._i2c_ctrl.read_word_data(self.addr, self.REG_TMP)
+        msb = data & 0x00FF
+        lsb = (data & 0xFF00) >> 8
+        word = (msb << 8) | lsb
+        temperature = word & 0x0FFF
+        temperature /= 16.0
+
+        if word & 0x1000:
+            temperature -= 256
+        return temperature
+
+    def temperature(self):
+        return self._temperature
+
+    def measure(self):
+        self._temperature = self.read_temperature()
+
+        return {
+            'temperature': self._temperature
+        }

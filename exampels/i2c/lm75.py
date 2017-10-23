a4a97a06fa6a909e6036d766c2da80f2e66bfeaa
Alexander Rüedlinger <a.rueedlinger@gmail.com>
2017-10-23 05:59:35 +0200

    add example code for lm75
    Signed-off-by: Alexander Rüedlinger <a.rueedlinger@gmail.com>

---
+++ exampels/i2c/lm75.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+from senlib.core.i2c import Controller
+from senlib.i2c.sensors.lmx import LM75
+import time
+
+if __name__ == '__main__':
+    i2c_ctrl = Controller(bus=1)
+    lm = LM75(i2c_ctrl=i2c_ctrl)
+    time.sleep(0.2)
+    data = lm.measure()
+    print(data)
+    time.sleep(0.2)

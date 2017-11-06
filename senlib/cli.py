4584dd1260ca5ea0a323b08ef6d818f0d5c48f69
Alexander Rüedlinger <a.rueedlinger@gmail.com>
2017-11-06 02:52:31 +0100

    use --adrdress instaed of --addr
    Signed-off-by: Alexander Rüedlinger <a.rueedlinger@gmail.com>

--- senlib/cli.py
+++ senlib/cli.py
@@ -7,7 +7,7 @@ import argparse
 def main():
     parser = argparse.ArgumentParser(description='senlib CLI')
     parser.add_argument('sensor', help='I2C sensor')
-    parser.add_argument('-a', '--addr', type=str, dest='addr', help='I2C address')
+    parser.add_argument('-a', '--address', type=str, dest='addr', help='I2C address')
     parser.add_argument('-b', '--bus', type=int, dest='bus', help='I2C bus')
     parser.add_argument('-d', '--debug', dest='debug', help='debug',
             action='store_true')

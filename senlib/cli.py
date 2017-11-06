9ce4be0f2db7ffaee59d20752763443e37ee2a13
Alexander Rüedlinger <a.rueedlinger@gmail.com>
2017-11-06 03:00:11 +0100

    improve existing cli
    Signed-off-by: Alexander Rüedlinger <a.rueedlinger@gmail.com>

--- senlib/cli.py
+++ senlib/cli.py
@@ -2,20 +2,49 @@
 
 __author__ = 'Alexander Rüedlinger'
 
+from collections import OrderedDict
 import argparse
+from .__version__ import __version__
+import json
+from xml.dom.minidom import parseString
+from dicttoxml import dicttoxml
+from datetime import datetime
+
 
 def main():
-    parser = argparse.ArgumentParser(description='senlib CLI')
-    parser.add_argument('sensor', help='I2C sensor')
-    parser.add_argument('-a', '--address', type=str, dest='addr', help='I2C address')
-    parser.add_argument('-b', '--bus', type=int, dest='bus', help='I2C bus')
-    parser.add_argument('-d', '--debug', dest='debug', help='debug',
+    parser = argparse.ArgumentParser(description='senlib CLI - Quick and dirty'
+            ' CLI for using some I2C sensors.')
+    parser.add_argument('--version', action='version', version='%(prog)s {}'
+            .format(__version__), help='Print version number.')
+    
+    parser.add_argument('sensor', help='Set I2C sensor driver.')
+    parser.add_argument('--mock', dest='mock', help='Use a mock sensor.', 
             action='store_true')
-    parser.add_argument('--mock', dest='mock', help='mock', action='store_true')
-    parser.add_argument('--http', dest='http', help='Run HTTP server',
+
+    parser.add_argument('-a', '--address', type=str, dest='addr', 
+        help='Set I2C address.')
+    parser.add_argument('-b', '--bus', type=int, dest='bus', help='Set I2C bus.')
+    parser.add_argument('-d', '--debug', dest='debug', help='Start debug mode.',
             action='store_true')
-    parser.add_argument('--http-address', type=str, dest='http_address', help='HTTP address', default='0.0.0.0')
-    parser.add_argument('--http-port', type=int, dest='http_port', help='HTTP port', default=8080)
+  
+    parser.add_argument('-p', '--poll', dest='poll', help='Start polling loop.',
+            action='store_true')
+    parser.add_argument('-i', '--interval', type=float, dest='interval', 
+            help='Set polling interval.', default=2)
+    parser.add_argument('-f', '--format', type=str, dest='format', 
+            help='Set output format.', choices=['json', 'xml', 'txt'], 
+            default='txt')
+    parser.add_argument('--pretty-print', dest='pretty_print', 
+        help='Use pretty print output format.', action='store_true')
+    parser.add_argument('-o', '--output', type=str, dest='output', 
+            help='Save output to a file.')
+
+    parser.add_argument('--http', dest='http', help='Run an HTTP server.',
+            action='store_true')
+    parser.add_argument('--http-address', type=str, dest='http_address', 
+            help='Set HTTP address.', default='0.0.0.0')
+    parser.add_argument('--http-port', type=int, dest='http_port', 
+            help='Set HTTP port.', default=8080)
     args = parser.parse_args()
 
     if args.debug:
@@ -25,7 +54,6 @@ def main():
     
     from senlib.i2c import DriverNotFound
     from senlib.i2c import get_sensor
-    from collections import OrderedDict
     
     try:
         addr = args.addr
@@ -39,16 +67,74 @@ def main():
         else:
             sensor = get_sensor(name=args.sensor, bus=args.bus, addr=addr)
         
-        print("sensor: {}".format(args.sensor.lower()))
-        for key, value in OrderedDict(sorted(sensor.measure().items())).items():
-            print("\t{}: {}".format(key, value))
-        print('')
-        
-        if args.http:
+        args.output_file = None
+        if args.output:
+            args.output_file = open(args.output, 'w+')
+       
+        if args.poll:
+            import asyncio
+            loop = asyncio.get_event_loop()
+            def callback():
+                data = sensor.measure()
+                print_output(args, sensor)
+                loop.call_later(args.interval, callback)
+
+            try:
+                callback()   
+                loop.run_forever()
+            except KeyboardInterrupt:
+                pass
+            finally:
+                loop.close()
+                if args.output_file:
+                    args.output_file.close()
+
+        elif args.http:
             from senlib.http import Server
             server = Server(sensor)
             server.run(args.http_address, args.http_port)
-
+        else:
+            print_output(args, sensor)
     except DriverNotFound as e:
         print(e)
 
+def print_output(args, sensor):
+    data = OrderedDict()
+    data['name'] = args.sensor.lower()
+    data['timestamp'] = str(datetime.now())
+    data['measurements'] = {}
+    sensor_data = sensor.measure()
+    for key, value in sorted(sensor_data.items()):
+       data['measurements'][key] = value
+
+    out = ''
+    if args.format == 'json':
+        out = print_json(args, data)
+    elif args.format == 'xml':
+        out = print_xml(args, data)
+    else:
+        out = print_txt(args, data)
+
+    print(out)
+    if args.output_file:
+        args.output_file.write(out)
+
+def print_json(args, data):
+    indent = 4 if args.pretty_print else None
+    json_data = json.dumps(data, indent=indent)
+    return json_data
+
+def print_xml(args, data):
+    xml = dicttoxml(data, attr_type=False, custom_root='sensor')
+    if args.pretty_print:
+        dom = parseString(xml)
+        return dom.toprettyxml()
+    else:
+        return xml
+
+def print_txt(args, data):
+    out = "sensor: {}\n".format(data['name'])
+    out += "timestamp: {}\n".format(data['timestamp'])
+    for key, value in data['measurements'].items():
+        out += "\t{}: {}\n".format(key, value)
+    return out

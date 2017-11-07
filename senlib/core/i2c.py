bbe024febc2325aead86f157c8bcc3102ea39e0f
Alexander Rüedlinger <a.rueedlinger@gmail.com>
2017-11-07 04:27:14 +0100

    add helper class LogController
    Signed-off-by: Alexander Rüedlinger <a.rueedlinger@gmail.com>

--- senlib/core/i2c.py
+++ senlib/core/i2c.py
@@ -1,7 +1,10 @@
 # -*- coding: utf-8 -*-
 
 __author__ = 'Alexander Rüedlinger'
-__all__ = ('Controller')
+__all__ = ('Controller', 'LogController')
+
+import logging
+logger = logging.getLogger('i2c')
 
 
 class ControllerInterface(object):
@@ -99,6 +102,97 @@ class Controller(ControllerInterface):
         return self._smbus.read_i2c_block_data(addr, cmd, nbytes)
 
     def write_i2c_block_data(self, addr, cmd, vals):
+        self._smbus.write_i2c_block_data(addr, cmd, vals)
+
+
+class LogController(ControllerInterface):
+    """
+    A helper class for using the Linux I2C/SMBus interface.
+    """
+
+    def __init__(self, bus=1, SMBus=None):
+        self._bus = bus
+        self._smbus = None
+        self._log_data = []
+        if SMBus is not None:
+            self._smbus = SMBus(bus)
+        else:
+            import smbus
+            self._smbus = smbus.SMBus(bus)
+
+        msg = 'create SMBus(bus={}) object'.format(bus)
+        self._log_data.append(msg)
+        logger.debug(msg)
+
+    @property
+    def name(self):
+        return 'i2c-{}'.format(self._bus)
+
+    @property
+    def bus(self):
+        return self._bus
+
+    def close(self):
+        self._smbus.close()
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        self.close()
+
+    def __enter__(self):
+        return self
+
+    def read_byte(self, addr):
+        r = self._smbus.read_byte(addr)
+        msg = 'call SMBus.read_byte(addr={})={}'.format(addr, r)
+        logger.debug()
+        self._log_data.append(msg)
+        logger.debug(msg)
+        return r
+
+    def write_byte(self, addr, val):
+        msg = 'call SMBus.write_byte(addr={}, val={})'.format(addr, val)
+        self._log_data.append(msg)
+        logger.debug(msg)
+        self._smbus.write_byte(addr, val)
+
+    def read_byte_data(self, addr, cmd):
+        r = self._smbus.read_byte_data(addr, cmd)
+        msg = 'call SMBus.read_byte_data(addr={}, cmd={})={}'.format(addr, cmd, r)
+        self._log_data.append(msg)
+        logger.debug(msg)
+        return r
+
+    def write_byte_data(self, addr, cmd, val):
+        msg = 'call SMBus.write_byte_data(addr={}, cmd={}, val={})'.format(addr, cmd, val)
+        self._log_data.append(msg)
+        logger.debug(msg)
+        self._smbus.write_byte_data(addr, cmd, val)
+
+    def read_word_data(self, addr, cmd):
+        r = self._smbus.read_word_data(addr, cmd)
+        msg = 'call SMBus.read_word_data(addr={}, cmd={})={}'.format(addr, cmd, r)
+        self._log_data.append(msg)
+        logger.debug(msg)
+        return r
+
+    def write_word_data(self, addr, cmd, val):
+        msg = 'call SMBus.write_word_data(addr={}, cmd={}, val={})'.format(addr, 
+                cmd, val)
+        self._log_data.append(msg)
+        logger.debug(msg)
+        self._smbus.write_word_data(addr, cmd, val)
+
+    def read_i2c_block_data(self, addr, cmd, nbytes):
+        r = self._smbus.read_i2c_block_data(addr, cmd, nbytes)
+        msg = 'call SMBus.read_i2c_block_data(addr={}, cmd={}, nbytes={})={}'.format(addr, cmd, nbytes, r)
+        self._log_data.append(msg)
+        logger.debug(msg)
+        return r
+
+    def write_i2c_block_data(self, addr, cmd, vals):
+        msg = 'call SMBus.write_i2c_block_data(addr={}, cmd={}, vals={})'.format(addr, cmd, vals)
+        self._log_data.append(msg)
+        logger.debug(msg)
         self._smbus.write_i2c_block_data(addr, cmd, vals)
 
 

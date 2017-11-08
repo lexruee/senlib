51a093f0315a4a153b606d50a4bdab4331dc583c
Alexander Rüedlinger <a.rueedlinger@gmail.com>
2017-11-08 06:32:52 +0100

    add tests for am2315/am2321
    Signed-off-by: Alexander Rüedlinger <a.rueedlinger@gmail.com>

---
+++ tests/test_amx.py
@@ -0,0 +1,60 @@
+# -*- coding: utf-8 -*-
+from senlib.i2c.sensors.amx import AM2315
+from senlib.i2c.sensors.amx import AM2321
+from senlib.core.i2c import MockController
+
+AM2315.MIN_SAMPLING_PERIOD = 0
+AM2321.MIN_SAMPLING_PERIOD = 0
+I2C_DATA_IN = [
+        [3, 4, 1, 241, 0, 203, 224, 112],
+        [3, 4, 1, 242, 0, 203, 16, 112],
+        [3, 4, 1, 243, 0, 203, 65, 176]
+]
+
+
+def test_create_am2315():
+    i2c_ctrl = MockController(read_data=I2C_DATA_IN)
+    sensor = AM2315(i2c_ctrl=i2c_ctrl)
+    assert sensor
+    return sensor
+
+def test_measure_am2315():
+    sensor = test_create_am2315()
+    sensor_data = sensor.measure()
+    assert sensor_data
+    assert sensor_data['temperature'] == 20.3
+    assert sensor_data['humidity'] == 49.7
+
+    sensor_data = sensor.measure()
+    assert sensor_data
+    assert sensor_data['temperature'] == 20.3
+    assert sensor_data['humidity'] == 49.8
+
+    sensor_data = sensor.measure()
+    assert sensor_data
+    assert sensor_data['temperature'] == 20.3
+    assert sensor_data['humidity'] == 49.9
+
+def test_create_am2321():
+    i2c_ctrl = MockController(read_data=I2C_DATA_IN)
+    sensor = AM2321(i2c_ctrl=i2c_ctrl)
+    assert sensor
+    return sensor
+
+def test_measure_am2321():
+    sensor = test_create_am2321()
+    sensor_data = sensor.measure()
+    assert sensor_data
+    assert sensor_data['temperature'] == 20.3
+    assert sensor_data['humidity'] == 49.7
+
+    sensor_data = sensor.measure()
+    assert sensor_data
+    assert sensor_data['temperature'] == 20.3
+    assert sensor_data['humidity'] == 49.8
+
+    sensor_data = sensor.measure()
+    assert sensor_data
+    assert sensor_data['temperature'] == 20.3
+    assert sensor_data['humidity'] == 49.9
+

6448f3b61cec211247dab91cdd44627e8fc3b793
Alexander Rüedlinger <a.rueedlinger@gmail.com>
2017-11-09 01:00:00 +0100

    adjust examples
    Signed-off-by: Alexander Rüedlinger <a.rueedlinger@gmail.com>

--- exampels/i2c/bmp280.py
+++ exampels/i2c/bmp280.py
@@ -1,10 +1,10 @@
 #!/usr/bin/env python3
-from senlib.core.i2c import Controller
+from senlib.core.i2c import SMBus
 from senlib.i2c.sensors.bmpx import BMP280
 import time
 
 if __name__ == '__main__':
-    i2c_ctrl = Controller(bus=1)
+    i2c_ctrl = SMBus(bus=1)
     sensor = BMP280(i2c_ctrl=i2c_ctrl)
     time.sleep(0.2)
     data = sensor.measure()
--- exampels/i2c/lm75.py
+++ exampels/i2c/lm75.py
@@ -1,10 +1,10 @@
 #!/usr/bin/env python3
-from senlib.core.i2c import Controller
+from senlib.core.i2c import SMBus
 from senlib.i2c.sensors.lmx import LM75
 import time
 
 if __name__ == '__main__':
-    i2c_ctrl = Controller(bus=1)
+    i2c_ctrl = SMBus(bus=1)
     sensor = LM75(i2c_ctrl=i2c_ctrl)
     time.sleep(0.2)
     data = sensor.measure()

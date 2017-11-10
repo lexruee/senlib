6881edaaf43dfb2ed3b742c7fbecf2dd19a28ffc
Alexander Rüedlinger <a.rueedlinger@gmail.com>
2017-11-10 23:53:31 +0100

    mcpx: add logging stmts
    Signed-off-by: Alexander Rüedlinger <a.rueedlinger@gmail.com>

--- senlib/i2c/sensors/mcpx.py
+++ senlib/i2c/sensors/mcpx.py
@@ -3,13 +3,15 @@
 __author__ = 'Alexander Rüedlinger'
 __all__ = ('MCP9808')
 
+import logging
+logger = logging.getLogger('mcpx')
 from senlib.core.i2c import Sensor as I2CSensor
 
 
 class MCP9808(I2CSensor):
     """
-    This is a software driver implementation for the Microchip MCP9808 temperature sensor
-    for use with Raspberry Pi computers.
+    This is a software driver implementation for the Microchip MCP9808 
+    temperature sensor for use with Raspberry Pi computers.
     """
 
     DRIVER_NAME = 'mcp9808'
@@ -26,6 +28,7 @@ class MCP9808(I2CSensor):
 
     def __init__(self, bus, addr=DEFAULT_ADDR):
         super(MCP9808, self).__init__(bus, addr)
+        logger.debug('create MCP9808(addr=%s) object', addr)
         self.id = self._read_device_id()
         self._temperature = 0.0
 
@@ -38,9 +41,11 @@ class MCP9808(I2CSensor):
         return cls.DEFAULT_ADDR
 
     def _read_device_id(self):
+        logger.debug('read device id')
         return self._bus.read_byte_data(self.addr, self.REG_DEVICE_ID)
 
     def read_temperature(self):
+        logger.debug('read temperature data')
         data = self._bus.read_word_data(self.addr, self.REG_TMP)
         msb = data & 0x00FF
         lsb = (data & 0xFF00) >> 8

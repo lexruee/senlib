b0d739144106f6a2d0e8c4e6423f3459b53eab6e
Alexander Rüedlinger <a.rueedlinger@gmail.com>
2017-11-11 01:27:07 +0100

    add tests for hdc1008
    Signed-off-by: Alexander Rüedlinger <a.rueedlinger@gmail.com>

---
+++ tests/test_hdcx.py
@@ -0,0 +1,33 @@
+# -*- coding: utf-8 -*-
+from senlib.i2c.sensors.hdcx import HDC1008
+from senlib.core.i2c import MockBus
+
+I2C_DATA_IN = [
+    95, 95, 145, 145,
+    96, 96, 149, 149,
+    96, 96, 161, 161
+]
+
+
+def test_create_hdc1008():
+    bus = MockBus(read_data=I2C_DATA_IN)
+    sensor = HDC1008(bus=bus)
+    assert sensor
+    return sensor
+
+def test_measure_hdc1008():
+    sensor = test_create_hdc1008()
+    sensor_data = sensor.measure()
+    assert sensor_data
+    assert sensor_data['temperature'] - 21.47 <= 0.1
+    assert sensor_data['humidity'] - 56.86 <= 0.1
+
+    sensor_data = sensor.measure()
+    assert sensor_data
+    assert sensor_data['temperature'] - 22.12 <= 0.1
+    assert sensor_data['humidity'] - 58.43 <= 0.1
+
+    sensor_data = sensor.measure()
+    assert sensor_data
+    assert sensor_data['temperature'] - 22.12 <= 0.1
+    assert sensor_data['humidity'] - 63.17 <= 0.1

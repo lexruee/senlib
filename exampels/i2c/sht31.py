c6acb026f2873dbe7ed932c9bcef87ab47929cd9
Alexander Rüedlinger <a.rueedlinger@gmail.com>
2017-11-16 07:08:28 +0100

    add example code for sht31 sensor
    Signed-off-by: Alexander Rüedlinger <a.rueedlinger@gmail.com>

---
+++ exampels/i2c/sht31.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+from senlib.core.i2c import SMBus
+from senlib.i2c.sensors.shtx import SHT31
+import time
+
+if __name__ == '__main__':
+    bus = SMBus(bus=1)
+    sensor = SHT31(bus=bus)
+    time.sleep(0.2)
+    data = sensor.measure()
+    print(data)
+    time.sleep(0.2)
